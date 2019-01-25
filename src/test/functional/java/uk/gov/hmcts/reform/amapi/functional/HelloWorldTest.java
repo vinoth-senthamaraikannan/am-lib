4d83ae0fa3bd598c46a8b76911a505f775e7352b
Anna Rompa <annarom@kainos.com>
2019-01-25 11:12:23 +0100

    add hello world test

--- src/test/functional/java/uk/gov/hmcts/reform/amapi/functional/HelloWorldTest.java
+++ src/test/functional/java/uk/gov/hmcts/reform/amapi/functional/HelloWorldTest.java
@@ -4,15 +4,13 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import static io.restassured.RestAssured.get;
-import static org.hamcrest.core.IsEqual.equalTo;
 
-public class HealthCheckTest extends RestAssuredTest {
+public class HelloWorldTest extends RestAssuredTest {
 
     @Test
     @Category(SmokeTest.class)
     public void healthcheck_returns_200() {
-        get("/health")
-            .then().statusCode(200)
-            .and().body("status", equalTo("UP"));
+        get("/")
+                .then().statusCode(200);
     }
 }
