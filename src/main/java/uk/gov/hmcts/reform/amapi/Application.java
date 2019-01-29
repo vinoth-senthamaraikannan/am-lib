ae6df24ad152b72c2ba02f5e119acc9facfed8c4
Remigiusz Kruk <R.Kruk@kainos.com>
2019-01-29 10:02:53 +0100

    add flyway migrations on startup

--- src/main/java/uk/gov/hmcts/reform/amapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/amapi/Application.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.amapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
@@ -7,9 +9,17 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 @SpringBootApplication
 @EnableCircuitBreaker
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private FlywayMigrator flywayMigrator;
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        flywayMigrator.runMigrations();
     }
 }
