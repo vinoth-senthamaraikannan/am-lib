8f627ecf977d508c558fe5e73afd6cb407be322f
Anna Rompa <annarom@kainos.com>
2019-01-30 15:42:16 +0100

    Add duration for readability

--- src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/scenarios/BasicScenarios.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/amlib/performance/scenarios/BasicScenarios.scala
@@ -5,18 +5,20 @@ import gov.hmcts.reform.amlib.performance.http.AccessManagement
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 
+import scala.concurrent.duration._
+
 object BasicScenarios {
   val helloWorld: ScenarioBuilder = scenario("Hello World")
     .forever(
       exec(AccessManagement.helloWorld)
-        .pause(1)
+        .pause(1.second)
     )
 
   val createResourceAccess: ScenarioBuilder = scenario("Create Resource Access")
     .forever(
       feed(CreateResourceAccess.feed)
         .exec(AccessManagement.createResourceAssess)
-        .pause(1)
+        .pause(1.second)
     )
 
   val filterResource: ScenarioBuilder = scenario("Filter Resource")
@@ -24,7 +26,7 @@ object BasicScenarios {
       feed(CreateResourceAccess.feed)
         .exec(AccessManagement.createResourceAssess)
         .exec(AccessManagement.filterResource)
-        .pause(1)
+        .pause(1.second)
     )
 
   val getAccessorsList: ScenarioBuilder = scenario("Filter Resource")
@@ -32,6 +34,6 @@ object BasicScenarios {
       feed(CreateResourceAccess.feed)
         .exec(AccessManagement.createResourceAssess)
         .exec(AccessManagement.getAccessorsList)
-        .pause(1)
+        .pause(1.second)
     )
 }
