9f454a0f92c2dc8ef27d97c56708e3fd843e27b6
Damian Dunajski <damian.dunajski@hmcts.net>
2019-02-25 15:29:44 +0000

    Add few breaking test cases, clenup old cases and unused resources

--- am-lib/src/test/java/uk/gov/hmcts/reform/amlib/FilterServiceTest.java
+++ am-lib/src/test/java/uk/gov/hmcts/reform/amlib/FilterServiceTest.java
@@ -16,7 +16,6 @@ import uk.gov.hmcts.reform.amlib.enums.Permission;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static uk.gov.hmcts.reform.amlib.helpers.TestConstants.CREATE_PERMISSION;
@@ -251,25 +250,110 @@ class FilterServiceTest {
         );
     }
 
-//    @Test
-//    void itShouldBePossibleToShowEverythingExceptOnePropertyOtherThanProperty() throws IOException {
-//        JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/input.json"));
-//
-//        Map<JsonPointer, Set<Permission>> attributePermissions = ImmutableMap.<JsonPointer, Set<Permission>>builder()
-//            .put(JsonPointer.valueOf(""), READ_PERMISSION)
-//            .put(JsonPointer.valueOf("/address"), CREATE_PERMISSION)
-//            .put(JsonPointer.valueOf("/address/city"), READ_PERMISSION)
-//            .build();
-//
-//        JsonNode returnedJson = fs.filterJson(inputJson, attributePermissions);
-//
-//        assertThat(returnedJson).isEqualTo(objectNode(node -> node
-//            .put("name", "John")
-//            .put("age", 18)
-//            .putObject("address")
-//            .put("city", "London")
-//        ));
-//    }
+    @Test
+    void itShouldBePossibleToShowManyTopLevelObjects() throws IOException {
+        JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/input.json"));
+
+        Map<JsonPointer, Set<Permission>> attributePermissions = ImmutableMap.<JsonPointer, Set<Permission>>builder()
+            .put(JsonPointer.valueOf("/claimant"), READ_PERMISSION)
+            .put(JsonPointer.valueOf("/defendant"), READ_PERMISSION)
+            .build();
+
+        JsonNode returnedJson = fs.filterJson(inputJson, attributePermissions);
+
+        assertThat(returnedJson).isEqualTo(mapper.valueToTree(
+            Resource.builder()
+                .claimant(Claimant.builder()
+                    .name("John")
+                    .age(21)
+                    .address(Address.builder()
+                        .city("London")
+                        .postcode("SE1")
+                        .build())
+                    .build()
+                )
+                .defendant(Defendant.builder()
+                    .name("Marry")
+                    .address(Address.builder()
+                        .city("Swansea")
+                        .postcode("SA1")
+                        .build())
+                    .build()
+                )
+                .build()
+            )
+        );
+    }
+
+    @Test
+    void itShouldBePossibleToShowEverythingExceptTopLevelObjectWithLeafLevelException() throws IOException {
+        JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/input.json"));
+
+        Map<JsonPointer, Set<Permission>> attributePermissions = ImmutableMap.<JsonPointer, Set<Permission>>builder()
+            .put(JsonPointer.valueOf(""), READ_PERMISSION)
+            .put(JsonPointer.valueOf("/claimant"), CREATE_PERMISSION)
+            .put(JsonPointer.valueOf("/claimant/address/city"), READ_PERMISSION)
+            .build();
+
+        JsonNode returnedJson = fs.filterJson(inputJson, attributePermissions);
+
+        assertThat(returnedJson).isEqualTo(mapper.valueToTree(
+            Resource.builder()
+                .claimant(Claimant.builder()
+                    .address(Address.builder()
+                        .city("London")
+                        .build())
+                    .build()
+                )
+                .defendant(Defendant.builder()
+                    .name("Marry")
+                    .address(Address.builder()
+                        .city("Swansea")
+                        .postcode("SA1")
+                        .build())
+                    .build()
+                )
+                .amount(100)
+                .build()
+            )
+        );
+    }
+
+    @Test
+    void itShouldBePossibleToShowEverythingExceptLeafLevelObjectWithChildException() throws IOException {
+        JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/input.json"));
+
+        Map<JsonPointer, Set<Permission>> attributePermissions = ImmutableMap.<JsonPointer, Set<Permission>>builder()
+            .put(JsonPointer.valueOf(""), READ_PERMISSION)
+            .put(JsonPointer.valueOf("/claimant/address"), CREATE_PERMISSION)
+            .put(JsonPointer.valueOf("/claimant/address/city"), READ_PERMISSION)
+            .build();
+
+        JsonNode returnedJson = fs.filterJson(inputJson, attributePermissions);
+
+        assertThat(returnedJson).isEqualTo(mapper.valueToTree(
+            Resource.builder()
+                .claimant(Claimant.builder()
+                    .name("John")
+                    .age(21)
+                    .address(Address.builder()
+                        .city("London")
+                        .build())
+                    .build()
+                )
+                .defendant(Defendant.builder()
+                    .name("Marry")
+                    .address(Address.builder()
+                        .city("Swansea")
+                        .postcode("SA1")
+                        .build())
+                    .build()
+                )
+                .amount(100)
+                .build()
+            )
+        );
+    }
 
     @Test
     void itShouldBePossibleToHideEverything() throws IOException {
@@ -280,50 +364,48 @@ class FilterServiceTest {
         assertThat(returnedJson).isNull();
     }
 
-    // REVIEW BELOW CASES
-
     @Test
-    void filterNestedJsonWhenRootExplicitlyHasNoReadButAnAttributeDoes() throws IOException {
-        final JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/filterServiceTestInput.json"));
-        final JsonNode expectedJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/filterServiceTestExpected.json"));
+    void itShouldBePossibleToShowLeafLevelValueOnlyWhenParentIsForbidden() throws IOException {
+        JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/input.json"));
 
-        final Map<JsonPointer, Set<Permission>> attributePermissions = new ConcurrentHashMap<>();
-        attributePermissions.put(JsonPointer.valueOf("/child"), CREATE_PERMISSION);
-        attributePermissions.put(JsonPointer.valueOf("/child/childName"), READ_PERMISSION);
+        Map<JsonPointer, Set<Permission>> attributePermissions = ImmutableMap.<JsonPointer, Set<Permission>>builder()
+            .put(JsonPointer.valueOf("/claimant"), CREATE_PERMISSION)
+            .put(JsonPointer.valueOf("/claimant/name"), READ_PERMISSION)
+            .build();
 
         JsonNode returnedJson = fs.filterJson(inputJson, attributePermissions);
 
-        assertThat(returnedJson).isEqualTo(expectedJson);
+        assertThat(returnedJson).isEqualTo(mapper.valueToTree(
+            Resource.builder()
+                .claimant(Claimant.builder()
+                    .name("John")
+                    .build()
+                )
+                .build()
+            )
+        );
     }
 
     @Test
-    void filterNestedJsonWithoutExplicitReadRemovesExpectedAttributes() throws IOException {
-        final JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/filterServiceTestInput.json"));
-        final JsonNode expectedJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/filterServiceTestExpected.json"));
+    void itShouldBePossibleToShowLeafLevelValueOnlyWhenSiblingIsForbidden() throws IOException {
+        JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/input.json"));
 
-        final Map<JsonPointer, Set<Permission>> attributePermissions = new ConcurrentHashMap<>();
-        attributePermissions.put(JsonPointer.valueOf("/child/childName"), READ_PERMISSION);
-        attributePermissions.put(JsonPointer.valueOf("/child/childAge"), CREATE_PERMISSION);
-
-        JsonNode returnedJson = fs.filterJson(inputJson, attributePermissions);
-
-        assertThat(returnedJson).isEqualTo(expectedJson);
-    }
-
-    @Test
-    void filterComplexNestedJson() throws IOException {
-        final JsonNode inputJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/filterServiceTestComplexInput.json"));
-        final JsonNode expectedJson = mapper.readTree(ClassLoader.getSystemResource("FilterServiceResources/filterServiceTestComplexExpected.json"));
-
-        final Map<JsonPointer, Set<Permission>> attributePermissions = new ConcurrentHashMap<>();
-        attributePermissions.put(JsonPointer.valueOf("/child"), READ_PERMISSION);
-        attributePermissions.put(JsonPointer.valueOf("/child/childAge"), CREATE_PERMISSION);
-        attributePermissions.put(JsonPointer.valueOf("/child/childName/firstName"), READ_PERMISSION);
-        attributePermissions.put(JsonPointer.valueOf("/child/childName/lastName"), CREATE_PERMISSION);
+        Map<JsonPointer, Set<Permission>> attributePermissions = ImmutableMap.<JsonPointer, Set<Permission>>builder()
+            .put(JsonPointer.valueOf("/claimant/name"), READ_PERMISSION)
+            .put(JsonPointer.valueOf("/claimant/age"), CREATE_PERMISSION)
+            .build();
 
         JsonNode returnedJson = fs.filterJson(inputJson, attributePermissions);
 
-        assertThat(returnedJson).isEqualTo(expectedJson);
+        assertThat(returnedJson).isEqualTo(mapper.valueToTree(
+            Resource.builder()
+                .claimant(Claimant.builder()
+                    .name("John")
+                    .build()
+                )
+                .build()
+            )
+        );
     }
 
     @Builder
